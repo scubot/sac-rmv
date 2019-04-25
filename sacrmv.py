a41f383876b8f9fdd9c02d86b9a38c88381678de
Anson <git@anson-cheung.com>
2019-04-25 10:08:50 +0100

    Fix minor issues

--- sacrmv.py
+++ sacrmv.py
@@ -11,12 +11,16 @@ class SACRMV(commands.Cog):
     async def air(self, ctx, start_pressure: int, end_pressure: int, bottom_time: int, average_depth: int, tank_sp: int,
                   tank_capacity: int):
         pressure = (average_depth * 0.099376) + 1
-        bars_consumed = end_pressure - start_pressure
+        bars_consumed = start_pressure - end_pressure
 
         sac = bars_consumed / bottom_time / pressure
         rmv = (tank_capacity / tank_sp) * sac
 
         embed = discord.Embed(title="SAC/RMV Calculation")
-        embed.add_field(name="Surface Air Consumption", value="{:0.2f}".format(sac))
-        embed.add_field(name="Respiratory Mean Volume", value="{:0.2f}".format(rmv))
+        embed.add_field(name="Surface Air Consumption", value="{:0.2f} bar/min".format(sac), inline=False)
+        embed.add_field(name="Respiratory Mean Volume", value="{:0.2f} litre/min".format(rmv), inline=False)
         await ctx.send(embed=embed)
+
+
+def setup(bot):
+    bot.add_cog(SACRMV(bot))
